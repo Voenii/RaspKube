cce5c6b41666f5215521af255e1b402ca104ef89
Voenii <45244033+Voenii@users.noreply.github.com>
2018-12-02 11:42:45 +0100

    changes

--- 00_Router/01_config_router.sh
+++ 00_Router/01_config_router.sh
@@ -1,3 +1,6 @@
+#Get Clone of Github Repository
+git clone https://github.com/Voenii/RaspKube/ RaspKube
+
 # sudo nano /etc/dhcpcd.conf
 # interface eth0
 # static ip_address=10.0.0.1/8
@@ -5,7 +8,7 @@
 # nolink
 
 sudo sh -c "echo 'interface eth0' >> /etc/dhcpcd.conf"
-sudo sh -c "echo 'static ip_address=10.0.0.1/8' >> /etc/dhcpcd.conf"
+sudo sh -c "echo 'static ip_address=172.16.0.1/16' >> /etc/dhcpcd.conf"
 sudo sh -c "echo 'static domain_name_servers=8.8.8.8,8.8.4.4' >> /etc/dhcpcd.conf"
 sudo sh -c "echo 'nolink' >> /etc/dhcpcd.conf"
 
@@ -16,7 +19,8 @@ sudo apt install dnsmasq -y
 sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.old
 
 # Make a new configuration file for dnsmasq
-sudo nano /etc/dnsmasq.conf
+#sudo nano /etc/dnsmasq.conf
+sudo cp ~/RaspKube/00_Router/dnsmasq/dnsmasq.conf /etc/dnsmasq.conf
 
 sudo sysctl net.ipv4.ip_forward=1
 
@@ -33,6 +37,20 @@ sudo apt install iptables-persistent -y
 sudo rm /etc/hostname -rf
 sudo sh -c "echo 'router01' >> /etc/hostname"
 sudo hostname router01
+
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+
+#if you want to have en GUI Router 
+sudo apt install -y raspberrypi-ui-mods
+sudo apt-get install -y rpi-chromium-mods
+
+#VNC Server
+sudo apt-get install - y realvnc-vnc-server realvnc-vnc-viewer
+
+#Partition
+sudo apt-get install -y gparted
 
 sudo reboot
 
--- 01_Initialize/01_Init.sh
+++ 01_Initialize/01_Init.sh
@@ -1,6 +1,3 @@
-sudo apt-get update
-sudo apt-get upgrade -y
-
 sudo dphys-swapfile swapoff
 sudo dphys-swapfile uninstall
 sudo update-rc.d dphys-swapfile remove
@@ -9,11 +6,16 @@ sudo update-rc.d dphys-swapfile remove
 
 #sudo nano /boot/cmdline.txt
 
+#gpu_mem=16 cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory smsc95xx.macaddr=b8:27:eb:00:00:02
 sudo sh -c "echo ' cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory' >> /boot/cmdline.txt"
 
 #add at end of line with space between and NO [Enter]: 
 #cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory
 #[Ctrl]+[o] => [Enter] => [Ctrl]+[x]
+
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get upgrade -y
 
 sudo apt-get install -y \
     apt-transport-https \
@@ -24,7 +26,10 @@ sudo apt-get install -y \
     git \
     python3-pip \
     apache2-utils \
+    ansible \
     sshpass 
+
+sudo apt-get update
 
 sudo reboot
 
